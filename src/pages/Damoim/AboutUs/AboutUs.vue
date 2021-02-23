55fccdc038bd2a6cb0d56485dc85bb12d312cf21
Incheol Jung <incheol.jung@navercorp.com>
2021-02-24 01:17:32 +0900

    update link

--- src/pages/Damoim/AboutUs/AboutUs.vue
+++ src/pages/Damoim/AboutUs/AboutUs.vue
@@ -1,12 +1,32 @@
 <template>
-  <div>
-    Hello, AboutUs
-  </div>
+  <table class="table table-bordered table-lg mt-lg mb-0">
+    <thead class="text-uppercase">
+    <tr>
+      <th>test</th>
+    </tr>
+    </thead>
+    <tbody>
+      <tr v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+      </tr>
+    </tbody>
+  </table>
 </template>
 
 <script>
 export default {
-  name: "AboutUs"
+  name: "AboutUs",
+  data() {
+    return {
+      items: [
+        {
+          id: 0
+        },
+        {
+          id: 1
+        }]
+    }
+  }
 }
 </script>
 
